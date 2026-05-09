e3e74d0cc1163209ed8b92a09abe9baacdd4bdb8
58157667 <58157667@qq.com>
2026-05-09 21:49:12 +0900

    Update app.js

--- yoyaku/src/main/resources/static/app.js
+++ yoyaku/src/main/resources/static/app.js
@@ -65,7 +65,7 @@ if (!emailRegex.test(email)) {
     try {
 
         const response = await fetch(
-            "http://localhost:8080/api/reservations",
+            "https://yoyaku8.onrender.com:8080/api/reservations",
             {
                 method: "POST",
 
