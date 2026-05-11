5dd79e195c00555b76d9c6533d96a9abbd0a002b
58157667 <58157667@qq.com>
2026-05-11 18:05:29 +0900

    Update MailService.java

--- yoyaku/src/main/java/com/example/demo/MailService.java
+++ yoyaku/src/main/java/com/example/demo/MailService.java
@@ -1,3 +1,41 @@
+//package com.example.demo;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class  {
+//
+//    @Autowired
+//    private JavaMailSender mailSender;
+//
+//    public void sendReservationMail(Reservation reservation) {
+//
+//        SimpleMailMessage message = new SimpleMailMessage();
+//
+//        // 公司邮箱
+//        message.setTo("hedan7965@gmail.com");
+//
+//        // 标题
+//        message.setSubject("新预约通知");
+//
+//        // 邮件内容
+//        String body =
+//                "收到新的预约。\n\n" +
+//                "姓名: " + reservation.getName() + "\n" +
+//                "邮箱: " + reservation.getEmail() + "\n" +
+//                "电话: " + reservation.getPhone() + "\n" +
+//                "预约日期: " + reservation.getReserveDate() + "\n" +
+//                "预约时间: " + reservation.getReserveTime() + "\n" +
+//                "服务项目: " + reservation.getService();
+//
+//        message.setText(body);
+//
+//        mailSender.send(message);
+//    }
+//}
 
 package com.example.demo;
 
@@ -27,6 +65,8 @@ public class MailService {
                         "UTF-8"
                 );
 
+        
+        
         // 必须是真实 Gmail
         helper.setFrom("hedan7965@gmail.com");
 
