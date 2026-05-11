ca12b23bbf288b2895b5048b0bf4b062ccc779f9
58157667 <58157667@qq.com>
2026-05-11 17:02:37 +0900

    Create MailService.java

---
+++ yoyaku/src/main/java/com/example/demo/MailService.java
@@ -0,0 +1,51 @@
+
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendReservationMail(Reservation reservation)
+            throws Exception {
+
+        MimeMessage message =
+                mailSender.createMimeMessage();
+
+        MimeMessageHelper helper =
+                new MimeMessageHelper(
+                        message,
+                        true,
+                        "UTF-8"
+                );
+
+        // 必须是真实 Gmail
+        helper.setFrom("hedan7965@gmail.com");
+
+        // 收件人
+        helper.setTo("hedan7965@gmail.com");
+
+        // 标题
+        helper.setSubject("新しい予約");
+
+        // 内容
+        helper.setText(
+                "名前: " + reservation.getName() + "\n" +
+                "電話: " + reservation.getPhone() + "\n" +
+                "メール: " + reservation.getEmail() + "\n" +
+                "日付: " + reservation.getReserveDate() + "\n" +
+                "時間: " + reservation.getReserveTime()+ "\n" +
+                "サービス: " + reservation.getService()
+        );
+
+        mailSender.send(message);
+    }
+}
